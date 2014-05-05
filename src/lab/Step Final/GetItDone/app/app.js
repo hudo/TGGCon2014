6e55ff81586a9cf45c99bfabf6c4d7c8fbd636e3
Igor Roncevic <ironcev@hotmail.com>
2014-05-05 19:46:43 +0200

    AngularJS added.

---
+++ src/lab/Step Final/GetItDone/app/app.js
@@ -0,0 +1,21 @@
+﻿var getItDone = {};
+
+(function () {
+    getItDone.app = angular.module("app", ["ngRoute", "ngResource", "ngSanitize"]);
+
+    getItDone.app.config(["$routeProvider", function($routeProvider) {
+            $routeProvider.when("/new", {
+                templateUrl: "/app/views/createTicket.html",
+                controller: "ticketController"
+            }).when("/edit/:articleId", {
+                templateUrl: "/app/views/createTicket.html",
+                controller: "ticketController"
+            }).when("/dashboard", {
+                templateUrl: "/app/views/dashboard.html",
+                controller: "dashboardController"
+            }).otherwise({
+                redirectTo: '/dashboard'
+            });;
+        }
+    ]);
+}());
